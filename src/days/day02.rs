d1e88aee45ee924cd81c4ca51656e31f19c8aa22
Chris <chrmiller@microsoft.com>
2022-12-02 11:53:56 -0800

    Linted

--- src/days/day02.rs
+++ src/days/day02.rs
@@ -3,11 +3,11 @@ use hashbrown::HashMap;
 lazy_static::lazy_static! {
   // Used in part 1
   static ref OUTCOME_LUT: HashMap<&'static str, u32> = HashMap::from([
-    ("A X", 1+3), 
-    ("A Y", 2+6), 
+    ("A X", 1+3),
+    ("A Y", 2+6),
     ("A Z", 3+0),
-    ("B X", 1+0), 
-    ("B Y", 2+3), 
+    ("B X", 1+0),
+    ("B Y", 2+3),
     ("B Z", 3+6),
     ("C X", 1+6),
     ("C Y", 2+0),
@@ -16,8 +16,8 @@ lazy_static::lazy_static! {
 
   // Rearranged OUTCOME_LUT to be used for part 2
   static ref DECISION_LUT: HashMap<&'static str, u32> = HashMap::from([
-    ("A Z", OUTCOME_LUT["A Y"]), 
-    ("A Y", OUTCOME_LUT["A X"]), 
+    ("A Z", OUTCOME_LUT["A Y"]),
+    ("A Y", OUTCOME_LUT["A X"]),
     ("A X", OUTCOME_LUT["A Z"]),
     ("B Z", OUTCOME_LUT["B Z"]),
     ("B Y", OUTCOME_LUT["B Y"]),
@@ -31,11 +31,5 @@ lazy_static::lazy_static! {
 #[inline(always)]
 pub fn day2() -> u32 {
   let contents = include_str!("../../inputs/day02.txt");
-  let results = contents
-    .split('\n')
-    .fold(0, |acc, x| {
-      acc + DECISION_LUT[x]
-    });
-    
-  results
+  contents.split('\n').fold(0, |acc, x| acc + DECISION_LUT[x])
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,2 @@
-
 mod days;
 pub use days::*;
